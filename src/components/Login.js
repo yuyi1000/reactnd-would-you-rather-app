a0946aac37f52266e6f9b0bcdbed242b7a5218c0
Yi <yy471014@ohio.edu>
2018-08-05 15:48:07 -0400

    add a middleware logger to log out actions and state change

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
+import { setAuthedUser } from '../actions/authedUser'
 
 class Login extends Component {
 
+
+  state = {
+    name: 'sarahedo',
+  }
+
+  handleChange = (e) => {
+    const name = e.target.value
+    this.setState({name})
+  }
+
+  handleSubmit = (e) => {
+    const { dispatch } = this.props
+    const authedUserId = this.state.name
+    e.preventDefault()
+    console.log(this.state.name)
+    dispatch(setAuthedUser(authedUserId))
+  }
+
   render() {
+    const { authedUser } = this.props
+    console.log(authedUser);
+    if (authedUser) {
+      // TODO: use redirect to home page
+      return (
+        <h2>
+          you have already signed in.
+        </h2>
+      )
+    }
 
     return (
-      <h2>Login</h2>
+      <form className='sign-in-form' onSubmit={this.handleSubmit}>
+        <label className='sign-in-label'>
+          Sign in
+          <select  value={this.state.value} onChange={this.handleChange}>
+            <option value="sarahedo">Sarah Edo</option>
+            <option value="tylermcginnis">Tyler McGinnis</option>
+            <option value="johndoe">John Doe</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     )
   }
 
