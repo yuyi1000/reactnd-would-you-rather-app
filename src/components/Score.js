9e96abd31ce9b85949cf39cad2cab52e1a88adc3
Yi <yy471014@ohio.edu>
2018-08-06 16:04:22 -0400

    decompose Leaderboard component into Score components

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { getUserScore, getNumberOfAnseredQuestions, getNumberOfCreatedQuestions } from '../utils/api'
+
 
 class Score extends Component {
 
-
   render() {
+    const { user } = this.props
+
     return (
       <h2>
-        this is score panel.
+        we have a user {user.name},
+        Answered questions: {getNumberOfAnseredQuestions(user)},
+        Created quetions: {getNumberOfCreatedQuestions(user)}, 
+        Total score: {getUserScore(user)}
       </h2>
     )
   }
 
 }
 
-export default connect()(Score)
+function mapStateToProps ({users}, {userId}) {
+  const user = users[userId]
+  return {
+    user,
+  }
+}
+
+
+export default connect(mapStateToProps)(Score)
