a0779d9fc752df5fda8e8275c88342ecb807021e
Yi <yy471014@ohio.edu>
2018-08-12 12:20:36 -0400

    add a reducer for action 'USER_SAVE_QUESTION'

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,4 @@
-import { RECEIVE_USERS, USER_ASK_QUESTION } from '../actions/users'
+import { RECEIVE_USERS, USER_ASK_QUESTION, USER_SAVE_QUESTION } from '../actions/users'
 
 export default function users (state = {}, action) {
   switch(action.type) {
@@ -13,6 +13,16 @@ export default function users (state = {}, action) {
         [action.user]: {
           ...state[action.user],
           questions: state[action.user].questions.concat(action.qid)
+        }
+      }
+    case USER_SAVE_QUESTION :
+      const { user, qid, answer } = action
+      return {
+        ...state,
+        [user]: {
+          ...state[user],
+          answers: ...state[user].answers,
+          [qid]: answer
         }
       }
     default :
