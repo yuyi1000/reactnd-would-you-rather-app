2acdca4d21129a005d1ca1031021dadd94ca1927
Yi <yy471014@ohio.edu>
2018-08-12 12:15:25 -0400

    add 'USER_SAVE_QUESTION' action

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,5 +1,6 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS'
 export const USER_ASK_QUESTION = 'USER_ASK_QUESTION'
+export const USER_SAVE_QUESTION = 'USER_SAVE_QUESTION'
 
 export function receiveUsers (users) {
   return {
@@ -13,5 +14,14 @@ export function userAskQuestion (user, qid) {
     type: USER_ASK_QUESTION,
     user,
     qid,
+  }
+}
+
+export function userSaveQuestion (user, qid, answer) {
+  return {
+    type: USER_SAVE_QUESTION,
+    user,
+    qid,
+    answer,
   }
 }
