a11d5614d3f673deff48f8046d7a33c3bb809050
Yi <yy471014@ohio.edu>
2018-08-14 10:57:33 -0400

    add style for question result

--- src/components/QuestionResult.js
+++ src/components/QuestionResult.js
@@ -5,7 +5,7 @@ import { Redirect } from 'react-router-dom'
 class QuestionResult extends Component {
 
   render() {
-    const { question, authedUser } = this.props
+    const { question, authedUser, name, avatarURL } = this.props
     if (authedUser === null) {
       alert('There is something wrong. Please login first.')
       return (
@@ -14,23 +14,38 @@ class QuestionResult extends Component {
     }
 
     return (
-      <div>
-        Name: {question.author},
-        OptionOne: {question.optionOne.text},
-        NumberOfOptionOne: {question.optionOne.votes.length},
-        OptionTwo: {question.optionTwo.text},
-        NumberOfOptionTwo: {question.optionTwo.votes.length}
+      <div className='question-result'>
+        <div className='question-result-name'>
+          Asked by {name}: <br/>
+        </div>
+        <div>
+          <div className='question-result-avatar'>
+            <img src={avatarURL} />
+          </div>
+          <div className='question-result-details'>
+            OptionOne: {question.optionOne.text},
+            NumberOfOptionOne: {question.optionOne.votes.length},
+            OptionTwo: {question.optionTwo.text},
+            NumberOfOptionTwo: {question.optionTwo.votes.length}
+          </div>
+        </div>
+
       </div>
+
     )
   }
 
 }
 
-function mapStateToProps ({ questions, authedUser }, { match }) {
+function mapStateToProps ({ questions, authedUser, users }, { match }) {
   const { question_id } = match.params
+  const question = questions[question_id]
+  const { name, avatarURL } = users[question.author]
   return {
-    question: questions[question_id],
+    question,
     authedUser,
+    name,
+    avatarURL,
   }
 }
 
